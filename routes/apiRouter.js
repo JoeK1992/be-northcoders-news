3ac6f9534a50e0159f2ebfd75f23664df75866ea
Joe Kelly <jpk17@hotmail.co.uk>
2021-05-06 00:39:50 +0100

    newAttemptedFix for JSON

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -5,7 +5,7 @@ const articlesRouter = require("./articlesRouter");
 const commentsRouter = require("./commentsRouter");
 const { fetchAllEndpoints } = require("../endpointsJSONfunction");
 
-apiRouter.route("").get(fetchAllEndpoints);
+apiRouter.route("/").get(fetchAllEndpoints);
 
 apiRouter.use("/topics", topicsRouter);
 
