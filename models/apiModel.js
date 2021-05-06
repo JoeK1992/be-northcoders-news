6007ed369464440e63accf6b8ae583cc4c9ea78f
Joe Kelly <jpk17@hotmail.co.uk>
2021-05-06 15:25:39 +0100

    back to parse

--- models/apiModel.js
+++ models/apiModel.js
@@ -2,6 +2,6 @@ const fs = require("fs").promises;
 
 exports.fetchApiJSON = () => {
   return fs.readFile("endpoints.json", "utf8").then((apiJSON) => {
-    return JSON.stringify(obj, null, 2)(apiJSON);
+    return JSON.parse(apiJSON);
   });
 };
