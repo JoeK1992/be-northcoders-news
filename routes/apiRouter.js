ba95770364f766838c8be59b7a74d5f3d000e0d7
Joe Kelly <jpk17@hotmail.co.uk>
2021-05-06 01:24:17 +0100

    removed apiRouter

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,12 +3,6 @@ const topicsRouter = require("./topicsRouter");
 const usersRouter = require("./usersRouter");
 const articlesRouter = require("./articlesRouter");
 const commentsRouter = require("./commentsRouter");
-const { fetchAllEndpoints } = require("../endpointsJSONfunction");
-
-apiRouter
-  .route("/")
-  .get((req, res) => res.send({ ok: true }))
-  .all(methodNotAllowed);
 
 apiRouter.use("/topics", topicsRouter);
 
